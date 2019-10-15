92c610477c4afab4d0a7a4211d63532c664d08d7
Prakrithi-Shetty <prakrithishetty8072@gmail.com>
2019-10-15 11:34:15 +0530

    On click function added

--- src/Clock/Clock.js
+++ src/Clock/Clock.js
@@ -1,18 +1,24 @@
 import React from 'react';
-
+import './Clock.css';
 
 class Clock extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {date: new Date()};
+    this.changeTime = this.changeTime.bind(this);
   }
 
+  changeTime(){
+    this.setState({
+      date: new Date()
+    });
+  }
 
 
   render(){
   return (
-    <div className="Clock">
+    <div className="Clock" onClick={this.changeTime}>
           current time is {this.state.date.toString()};
     </div>
   );
