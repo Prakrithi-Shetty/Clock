4c376684ed5e145a3185ff707f1fe8d6af055326
Prakrithi-Shetty <prakrithishetty8072@gmail.com>
2019-10-20 20:34:08 +0530

    On change new name

--- src/InputDisplay/InputDisplay.js
+++ src/InputDisplay/InputDisplay.js
@@ -2,19 +2,32 @@ import React from 'react';
 
 
 class InputDisplay extends React.Component {
-    constructor(prpos){
+    constructor(props){
         super(props);
         this.state = { 
-            name:"prakrithi"
+            name:"prakrithi",
+            age:25
         };
+        this.myRef = React.createRef();
+        this.enteredName=this.enteredName.bind(this);
+    }
+
+     enteredName(e){
+         console.log( this.myRef.current.value);
+         const newName = this.myRef.current.value;
+         this.setState({
+            name:newName,
+         });
 
     }
     render()
     {
         return(
             <div>
-                {this.state.name}
-                <input  type="textbox" />
+                hi my name is {this.state.name} and my age is {this.state.age}
+                <div>
+                    <input  ref={this.myRef} type="textbox" onChange={this.enteredName}/>
+                </div>
             </div>
         );
     }
